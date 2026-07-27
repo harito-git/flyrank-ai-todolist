0bee2bef499bddf2a28d2c3f47e0519f67038010
Harit Oza <146392612+harito-git@users.noreply.github.com>
2026-07-27 08:34:38 -0400

    Integrate SQLite database for task management into main file

--- primary-repo-files/index.js
+++ primary-repo-files/index.js
@@ -2,12 +2,38 @@ import express from 'express';
 import path from 'path';
 const PORT = 3000;
 const app = express();
+
+//create database, databse is our object, taks.db is where our database, your data live sin a fiel called tasks.db.
+import Database from 'better-sqlite3';
+const db = new Database('tasks.db');
+//Checkpoint 5:
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocument from './openapi.json' with {"type":"json"};
+
+//Express needs this to aprse JSON bodies.
 app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const port = 3000;
+app.use(express.json());
+let tasks_list = 0;
 
-let to_do_list = [{id: 1, title: 'Room Cleanup', done:false}, {id:2, title: 'Groceries', done:false}, {id:3, title:'Attend flyrank backend session', done:true}]
+db.exec('CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY, title varchar(200), done bool)');
+
+//sue a transaction function
+const insert_3_objects = db.transaction((objs) => {
+    const count = db.prepare('SELECT COUNT(*) as count from tasks').get().count;
+    if(count == 0){
+        for(const obj of objs) 
+            db.prepare('INSERT or REPLACE into tasks(title, done) VALUES (@title, @done)').run(obj);
+    }
+})
+
+insert_3_objects([
+    {title: 'Wash hands', done:0},
+    {title: 'Plan your schedule', done:0},
+    {title: 'Clean room', done:1}
+]);
+
 
 app.get('/', (req, res) => {
     res.json({name: "Task API", version:"1.0", endpoints:["/tasks"]});
@@ -20,12 +46,16 @@ app.get('/health', (req, res) => {
 
 
 
+
+
+
+
 //Get tasks api endpoint crud task 2
 //get all tasks
 
 app.get('/tasks', (req, res) => {
-    const tasks = JSON.stringify(to_do_list);
-    res.status(200).json(to_do_list);
+    const tasks = db.prepare('SELECT * from tasks').all();
+    res.status(200).json(tasks);
 });
 
 //First task
@@ -35,9 +65,12 @@ app.get('/tasks/:id', (req, res) => {
     //parseInt() function covnerts a string to an integer
     const id_to_integer = parseInt(urlID, 10);
     //find an integer
-    const found = to_do_list.find((n) => n.id === id_to_integer);
-    if(found !== undefined){
-        res.status(200).json(found);
+    const found = db.prepare('SELECT * from tasks where id = ?');
+    const found_number = found.get(id_to_integer);
+    if(found_number !== undefined){
+        console.log(found_number);
+        return res.status(200).json(found_number);
+
 
     }
     else{
@@ -51,66 +84,74 @@ app.get('/tasks/:id', (req, res) => {
 app.post('/tasks', (req, res) => {
     const { title } = req.body;
     const title_to_string = String(title);
-    const title_found = to_do_list.find((t)=> t.title === title_to_string);
-    let tasks_list = Math.max(to_do_list.length, tasks_list);
-    if(title_found === undefined){
-        to_do_list.push({id: tasks_list, title: title_to_string, done:false});
+    const title_found = db.prepare('SELECT title from tasks where title = ?');
+    const find_title = title_found.get(title_to_string);
+    const find_total_number_of_items = db.prepare('SELECT COUNT(title) from tasks');
+    const number_of_items = find_total_number_of_items.get();
+    tasks_list = Math.max(find_total_number_of_items, tasks_list);
+    if(find_title === undefined){
+        const added_query = db.prepare('INSERT INTO tasks (title, done) VALUES (?, ?)');
+        const newtask = added_query.run(title, 0);
         tasks_list+=1;
-        const newTaskAdded = to_do_list.find((t) => t.title === title_to_string);
-        return res.status(201).json(newTaskAdded);
+        const added_new_task = db.prepare('SELECT * from tasks where title = ?');
+        const new_titles = added_new_task.get(title_to_string);
+        return res.status(201).json(new_titles);
     }
     else{
         return res.status(400).json("Bad request, please create a new task, title is missing or empty or already in tasks");
     }
 });
 
-//update tasks
+//checkpoint3: update tasks
 app.put('/tasks/:id', (req, res) => {
     const {title, done} = req.body;
     const title_to_string = String(title);
     const urlID = req.params.id;
-    const done_to_boolean = Boolean(done); 
+    const done_to_boolean = Number(done); 
      //parseInt() function covnerts a string to an integer
      const id_to_integer = parseInt(urlID, 10);
      //find an integer
-     const found = to_do_list.find((n) => n.id === id_to_integer);
+     const title_found = db.prepare('SELECT id from tasks where id = ?');
+     const find_title = title_found.get(id_to_integer);
+     
 
 
      //check for id in put request to update title and done
-    if(found !== undefined && title !== undefined && done !== undefined){
-        found.title = String(title);
-        found.done = Boolean(done);
-        return res.status(201).json(found);
+    if(find_title !== undefined && (title_to_string !== undefined || done_to_boolean !== undefined)){
+        const update_tasks = db.prepare('UPDATE tasks SET title = ?, done = ? WHERE id = ?');
+        const update_new_tasks = update_tasks.run(title_to_string, done_to_boolean, id_to_integer);
+        console.log(update_new_tasks.changes);
+        return res.status(201).json(update_new_tasks);
     }
-    else if(found !== undefined & (title === undefined || done === undefined)){
+    else if(find_title !== undefined && (title_to_string === undefined || done_to_boolean === undefined)){
         return res.status(400).json('Empty/invalid body, cannot update task.');
     }
-    else if(found === undefined){
+    else if(find_title === undefined){
         return res.status(404).json('Error 404, unknown id, not found in to-do list.');
     }
 });
 
 
-//delete task
+//checkpioint 4: delete task
 app.delete('/tasks/:id', (req, res) => {
-   //get id from parameters
-   const urlID = req.params.id;
-   //parseInt() function covnerts a string to an integer
-   const id_to_integer = parseInt(urlID, 10);
-   //find an integer
-   const found = to_do_list.find((n) => n.id === id_to_integer);
-   if(found !== undefined){
-        const index_to_be_removed = to_do_list.indexOf(found);
-        to_do_list.splice(index_to_be_removed, 1);
+    //get id from parameters
+    const urlID = req.params.id;
+    //parseInt() function covnerts a string to an integer
+    const id_to_integer = parseInt(urlID, 10);
+    const find_task_to_delete = db.prepare('SELECT id from tasks where id = ?');
+    const find_task_to_delete_get = find_task_to_delete.get(id_to_integer);
+    if(find_task_to_delete !== undefined){
+        //parseInt() function covnerts a string to an integer
+        const delete_task = db.prepare('DELETE FROM tasks WHERE id = ?');
+        const run_tasks = delete_task.run(id_to_integer);
         return res.status(204).json('No Content');
-
-   } 
-   else{
-        return res.status(404).json('Try again, unknown id.');
-   }
+    }
+    else{
+        return res.status(404).json('Unknown, no id is not found.');
+    }
 })
+
 
 app.listen(PORT, () => {
-    console.log('Listening at http://localhost:3000/docs');
-})
-
+    console.log('Listening at http://localhost:3000');
+});
